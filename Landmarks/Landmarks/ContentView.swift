e6c361b8656e89d14ccbcaceffdff0ee71eac61c
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-01-14 15:30:59 +0900

    SwiftUI

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by KyungHeon Lee on 2023/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Hello ,SwiftUI!")
+            .font(.title)
+            .foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by KyungHeon Lee on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
