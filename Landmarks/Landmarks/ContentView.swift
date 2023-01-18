5177d77db178b4309782e21f6c82fcc008434c5a
BOLTB0X <khunlee0508@gmail.com>
2023-01-18 23:06:44 +0900

    commit

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -7,11 +7,30 @@
 
 import SwiftUI
 
+// View를 상속받는 ContentView 구조체
 struct ContentView: View {
     var body: some View {
+        // 10개 이상 하려면
+        Form {
             Text("Hello ,SwiftUI!")
-            .font(.title)
-            .foregroundColor(.red)
+            Group {
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+            }
+            Group {
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+                Text("Hello ,SwiftUI!")
+            }
+            // 그냥 Text를 10 개 이상 선언시 에러
+            // 최상위 View는 최대 10 Child View
+                .font(.title)
+                .foregroundColor(.red)
+        }.navigationTitle("this is Title")
     }
 }
 
