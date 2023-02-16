eb4be2115ea2d2ae45189f370dc97e5415312bca
BOLTB0X <khunlee0508@gmail.com>
2023-02-16 23:26:18 +0900

    commit

---
+++ PracticeAVFoundation/PracticeAVFoundation/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PracticeAVFoundation
+//
+//  Created by KyungHeon Lee on 2023/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CameraView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticeAVFoundation/PracticeAVFoundation/PracticeAVFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAVFoundationApp.swift
+//  PracticeAVFoundation
+//
+//  Created by KyungHeon Lee on 2023/02/16.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAVFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
