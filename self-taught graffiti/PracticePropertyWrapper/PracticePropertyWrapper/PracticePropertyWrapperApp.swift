cc7cd735c0fe019c7366297e3d549efe79684d41
BOLTB0X <khunlee0508@gmail.com>
2023-03-13 23:43:21 +0900

    commit

---
+++ self-taught graffiti/PracticePropertyWrapper/PracticePropertyWrapper/PracticePropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePropertyWrapperApp.swift
+//  PracticePropertyWrapper
+//
+//  Created by KyungHeon Lee on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct PracticePropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
