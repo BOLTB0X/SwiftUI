ac1974508234668cc98064f40b1ba1314af20c97
BOLTB0X <khunlee0508@gmail.com>
2023-03-17 22:29:03 +0900

    Handling errors

--- iOS App Dev Tutorials/Chapter 6 Persistence and concurrency/05_Handling errors/Scrumdinger/ErrorView.swift
+++ iOS App Dev Tutorials/Chapter 6 Persistence and concurrency/05_Handling errors/Scrumdinger/ErrorView.swift
@@ -10,26 +10,42 @@ import SwiftUI
 struct ErrorView: View {
     let errorWrapper: ErrorWrapper // Wrapper property.
     
+    // @Environment 속성 래퍼를 사용하면 뷰의 프레젠테이션 모드, 장면 단계, 가시성 또는 색 구성표와 같이 뷰의 환경이 저장하는 값을 읽을 수 있음
+    // 뷰의 해제 구조에 액세스하고 뷰를 해제하는 함수처럼 호출
+    @Environment(\.dismiss) private var dismiss
+
+    
     var body: some View {
-        VStack {
-            Text("An error has occurred!")
-                .font(.title)
-                .padding(.bottom)
-            // 헤드라인 글꼴로 오류 설명을 표시하려면 텍스트 보기를 추가
-            // 오류는 현지화된 문자열 설명을 제공
-            
-            Text(errorWrapper.error.localizedDescription)
-                .font(.headline)
-            // 캡션 글꼴 및 패딩과 함께 지침을 표시하는 텍스트 보기를 추가
-            Text(errorWrapper.guidance)
-                           .font(.caption)
-                           .padding(.top)
-            Spacer()
+        NavigationView {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                // 헤드라인 글꼴로 오류 설명을 표시하려면 텍스트 보기를 추가
+                // 오류는 현지화된 문자열 설명을 제공
+                
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                // 캡션 글꼴 및 패딩과 함께 지침을 표시하는 텍스트 보기를 추가
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            // 배경을 흐리게 하면 이 오류 보기에 주의가 집중
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .navigationBarTitleDisplayMode(.inline)
+            // 하나의 도구 모음 항목으로 도구 모음을 만듬
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
         }
-        .padding()
-        // 배경을 흐리게 하면 이 오류 보기에 주의가 집중
-        .background(.ultraThinMaterial)
-        .cornerRadius(16)
     }
 }
 
--- iOS App Dev Tutorials/Chapter 6 Persistence and concurrency/05_Handling errors/Scrumdinger/ScrumdingerApp.swift
+++ iOS App Dev Tutorials/Chapter 6 Persistence and concurrency/05_Handling errors/Scrumdinger/ScrumdingerApp.swift
@@ -7,7 +7,9 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
-    
+    // ScrumdingerApp.swift에서 errorWrapper라는 선택적 상태 변수를 추가
+    @State private var errorWrapper: ErrorWrapper?
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
@@ -16,7 +18,8 @@ struct ScrumdingerApp: App {
                         do {
                             try await ScrumStore.save(scrums: store.scrums)
                         } catch {
-                            fatalError("Error saving scrums.")
+                            // 치명적인 오류 호출을 새 errorWrapper에 대한 할당으로 바꿈
+                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
                         }
                     }
                 }
@@ -25,8 +28,18 @@ struct ScrumdingerApp: App {
                 do {
                     store.scrums = try await ScrumStore.load()
                 } catch {
-                    fatalError("Error loading scrums.")
+                    // 치명적인 오류 호출을 새 errorWrapper에 대한 할당으로 바꿈
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
                 }
+            }
+            // 오류 래퍼 항목에 바인딩이 있는 시트를 추가
+            // 모달 시트는 사용자가 모달 시트를 닫을 때 코드를 실행하는 클로저와 모달로 표시할 뷰를 제공하는 클로저를 제공
+            .sheet(item: $errorWrapper, onDismiss: {
+                // onDismiss 클로저에서 스크럼 배열에 샘플 데이터를 할당
+                store.scrums = DailyScrum.sampleData
+            }) { wrapper in
+                ErrorView(errorWrapper: wrapper)
+
             }
         }
     }
